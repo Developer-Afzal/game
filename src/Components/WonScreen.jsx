8a2a0e135ba13b9fae55d46175cee6afbda9dc97
Developer-Afzal <afzalansari891@gmail.com>
2024-06-10 22:03:22 +0530

    sound updated

--- src/Components/WonScreen.jsx
+++ src/Components/WonScreen.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-
+import Win from '../assets/win.mp3'
 const WonScreen = ({closemodel, datavalue}) => {
+  if(datavalue === "No"){
+    const play_audio  = new Audio(Win)
+          play_audio.play()
+  }
   return (
    <div className='wonscreen-Block _Flex'>
         <h1>
